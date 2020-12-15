d0044b5b58c0eb96c6a67965bb291814a58ff3e8
PhilipNichols <philip.benjamin.nichols@gmail.com>
2020-12-15 15:34:00 +0000

    Created deko qa tech task. A java-selenium based robot framework

---
+++ src/test/java/com/deko/tests/RetailFinance/BackofficeLoginTest.java
@@ -0,0 +1,45 @@
+package com.deko.tests.RetailFinance;
+
+import com.deko.testing.robot.backoffice.BackofficeRobot;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BackofficeLoginTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void testTearDown(){
+        driver.close();
+    }
+
+    @AfterSuite
+    public void suiteTearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void backOfficeLoginPageLoadSuccess(){
+        BackofficeRobot robot = new BackofficeRobot(driver);
+        robot
+                .openBackofficeLoginPage()
+                .verifyBackofficeUrl();
+
+        Assert.assertTrue(robot.verifyBackofficeUrl());
+    }
+
+    //todo: Write the rest of the tests for the backoffice login page here.
+    //todo: Chrome driver should spin up, pass all tests identified (unless you find a bug?) and quit.
+}
