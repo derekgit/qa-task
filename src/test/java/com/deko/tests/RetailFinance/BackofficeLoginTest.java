fba1cbd4c81eff7c8a046b421ff64c5110f252d1
Derek <derekhusbands0@gmail.com>
2021-02-02 19:04:45 +0000

    3 tests added Successful login, Invlaid username and invalid Password

--- src/test/java/com/deko/tests/RetailFinance/BackofficeLoginTest.java
+++ src/test/java/com/deko/tests/RetailFinance/BackofficeLoginTest.java
@@ -30,7 +30,7 @@ public class BackofficeLoginTest {
         driver.quit();
     }
 
-    @Test
+    @Test(priority=0)
     public void backOfficeLoginPageLoadSuccess(){
         BackofficeRobot robot = new BackofficeRobot(driver);
         robot
@@ -42,4 +42,68 @@ public class BackofficeLoginTest {
 
     //todo: Write the rest of the tests for the backoffice login page here.
     //todo: Chrome driver should spin up, pass all tests identified (unless you find a bug?) and quit.
+
+
+    //Postive Test.
+
+    @Test(priority=1)
+    public void backOfficeLoginPageSuccess(){
+
+        BackofficeRobot robot = new BackofficeRobot(driver);
+        robot
+                //open login page
+                .openBackofficeLoginPage()
+                //enter valid username
+                .fillLoginUsername("Derek.Husbands")
+                //enter valid password
+                .fillLoginPassword("DekoQA2020")
+                //submit login
+                .submitLoginForm();
+
+        //Check user has been logged in Successfully
+        Assert.assertTrue(robot.verifySuccessfulLogin());
+    }
+
+    //Negative Tests
+    @Test(priority=2)
+    public void backOfficeLoginPageInvalidUsername(){
+
+        BackofficeRobot robot = new BackofficeRobot(driver);
+        robot
+                //open login page
+                .openBackofficeLoginPage()
+                //enter invalid username
+                .fillLoginUsername("Derek.Husband")
+                //enter valid password
+                .fillLoginPassword("DekoQA2020")
+                //submit login
+                .submitLoginForm();
+
+        //Check Error Message is displayed
+        Assert.assertTrue(robot.verifySignInError());
+
+    }
+
+    @Test(priority=3)
+    public void backOfficeLoginPageInvalidPassword(){
+
+        BackofficeRobot robot = new BackofficeRobot(driver);
+        robot
+                //open login page
+                .openBackofficeLoginPage()
+                //enter valid username
+                .fillLoginUsername("Derek.Husbands")
+                //enter ivalid password
+                .fillLoginPassword("DekoQA202")
+                //submit login
+                .submitLoginForm();
+
+        //Check Error Message is displayed
+        Assert.assertTrue(robot.verifySignInError());
+
+    }
+
 }
+
+
+
